3dc85896d5cc90e742b2176b14971cfe027de36f
Thiago Sena <lord.sena@gmail.com>
2022-08-15 23:09:37 -0300

    feat: create route table association

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -38,3 +38,8 @@ resource "aws_route" "default_route" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.dev_internet_gateway.id
 }
+
+resource "aws_route_table_association" "dev_public_assoc" {
+  subnet_id      = aws_subnet.dev_public_subnet.id
+  route_table_id = aws_route_table.dev_public_rt.id
+}
