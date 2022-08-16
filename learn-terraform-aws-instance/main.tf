994858da9c69e0de087aa7274b2da1bdffb086fb
Thiago Sena <lord.sena@gmail.com>
2022-08-15 23:13:04 -0300

    feat: create datasource ami ubuntu and ec2 instance

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -74,3 +74,20 @@ resource "aws_key_pair" "dev_auth" {
   key_name   = "devkey"
   public_key = file("~/.ssh/devkey.pub")
 }
+
+resource "aws_instance" "dev_node" {
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.server_ami.id
+  key_name               = aws_key_pair.dev_auth.id
+  vpc_security_group_ids = [aws_security_group.dev_sg.id]
+  subnet_id              = aws_subnet.dev_public_subnet.id
+  user_data              = file("userdata.tpl")
+
+  root_block_device {
+    volume_size = 10
+  }
+
+  tags = {
+    Name = "dev-node"
+  }
+}
