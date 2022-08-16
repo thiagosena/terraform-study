7518083385bab98fe12477f8751fb442523831f0
Thiago Sena <lord.sena@gmail.com>
2022-08-15 23:06:57 -0300

    feat: create public subnet

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -7,3 +7,14 @@ resource "aws_vpc" "dev_vpc" {
     Name = "dev"
   }
 }
+
+resource "aws_subnet" "dev_public_subnet" {
+  vpc_id                  = aws_vpc.dev_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-west-2a"
+
+  tags = {
+    Name = "dev-public"
+  }
+}
